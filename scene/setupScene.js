bf4ac7dc16bfb165e03e76a53c9dab916651c5a7
Cas <73029218+casvde@users.noreply.github.com>
2025-06-11 22:33:18 +0200

    Add files via upload

--- scene/setupScene.js
+++ scene/setupScene.js
@@ -8,25 +8,53 @@ export function setupScene() {
   const backgroundTexture = loader.load('images/bg.png');
   scene.background = backgroundTexture;
 
-  const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
+  // Camera setup
+  const camera = new THREE.PerspectiveCamera(
+    50,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
   camera.position.set(2, 2, 5);
 
-  const renderer = new THREE.WebGLRenderer();
+  const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
-
   const texture = loader.load('images/logo.png');
   const material = new THREE.MeshBasicMaterial({ map: texture, transparent: true });
-  const geometry = new THREE.PlaneGeometry(1, 1); // Adjust size as needed
+
+  const logoWidth = 2;
+  const logoHeight = 2;
+  const geometry = new THREE.PlaneGeometry(logoWidth, logoHeight);
   const imagePlane = new THREE.Mesh(geometry, material);
-  imagePlane.position.set(-0.444, -0.32, -0.9);
-  camera.add(imagePlane); 
+
+
+  function positionLogo() {
+    const distance = 2; 
+    const vFOV = THREE.MathUtils.degToRad(camera.fov);
+    const height = 2 * Math.tan(vFOV / 2) * distance;
+
+    const width = height * camera.aspect;
+
+
+    const x = -width / 2 + logoWidth / 2 - 0.12;
+    const y = -height / 2 + logoHeight / 2 - 0.79;
+    const z = -distance;
+
+    imagePlane.position.set(x, y, z);
+  }
+
+  camera.add(imagePlane);
   scene.add(camera);
+  positionLogo();
+
+  window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    positionLogo();
+  });
 
   return { scene, camera, renderer };
-
-
-  
 }
-
